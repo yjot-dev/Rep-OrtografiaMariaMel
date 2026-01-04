203007d7f80c4e8f4a0798fc42680b58d2e47567
yjot <2010guabo@gmail.com>
2026-01-04 16:34:58 -0500

    v3 Test & AndroidTest

--- app/src/androidTest/java/com/yjotdev/ortografiamariamel/utils/di/DiModulesTest.kt
+++ app/src/androidTest/java/com/yjotdev/ortografiamariamel/utils/di/DiModulesTest.kt
@@ -1,12 +1,7 @@
 package com.yjotdev.ortografiamariamel.utils.di
 
-import android.content.Context
-import androidx.navigation.compose.ComposeNavigator
-import androidx.navigation.testing.TestNavHostController
 import dagger.Module
-import dagger.Provides
 import dagger.Binds
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import dagger.hilt.testing.TestInstallIn
 import javax.inject.Singleton
@@ -26,13 +21,4 @@ abstract class DiModulesTest {
     abstract fun bindFakeGamesRepository(
         impl: FakeGamesRepository
     ): GamesPort
-
-    companion object {
-        @Provides
-        @Singleton
-        fun provideTestNavHostController(@ApplicationContext context: Context) =
-            TestNavHostController(context).apply {
-                navigatorProvider.addNavigator(ComposeNavigator())
-            }
-    }
 }
