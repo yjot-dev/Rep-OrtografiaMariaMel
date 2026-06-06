693f5e14df38cc6910a21075abfd7539cf616797
yjot2 <2010guabo@gmail.com>
2026-06-06 15:01:36 -0500

    Subida de codigo de version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.yjotdev.ortografiamariamel"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.6"
+        versionCode = 7
+        versionName = "1.7"
         testInstrumentationRunner = "com.yjotdev.ortografiamariamel.CustomTestRunner"
         vectorDrawables {
             useSupportLibrary = true
