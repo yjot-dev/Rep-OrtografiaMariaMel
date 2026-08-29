1c091268872d3eb275419d99d52f4e3f207efaaa
yjot2 <2010guabo@gmail.com>
2026-08-29 17:53:51 -0500

    Actualización de dependencias y recursos webp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,7 +100,7 @@ dependencies {
     testImplementation(libs.mockk)
     testImplementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.androidx.test.espresso.core)
+    androidTestImplementation(libs.androidx.test.runner)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     androidTestImplementation(libs.dagger.hilt.android.testing)
